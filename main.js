5f2937ccc0b487cacc658c3366015a4e27948448
KevinAlexanderP <kennedyisdead13@gmail.com>
2020-01-23 12:36:17 -0600

    flexed

--- main.js
+++ main.js
@@ -18,32 +18,29 @@ function initMap() {
         })
     }
 
-    function getLocation(function(position) {
+    getLocation(function(position) {
+        // coordenadas centradas en el mapa // position coords
         var myMapCenter = { lat: position.coords.latitude, lng: position.coords.longitude };
+        // mapa , map and zoom
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: myMapCenter,
+            zoom: 14
+        });
+        // this is how we setup our request, #radius is adjustable also
 
-    })
+        var request = { location: myMapCenter, radius: '5000', name: 'Krispy Kreme' };
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, function(results, status) {
+            console.log(results, status)
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    var place = results[i];
+                    createMarker(results[i], map);
+                }
 
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        });
+    });
 
 }
